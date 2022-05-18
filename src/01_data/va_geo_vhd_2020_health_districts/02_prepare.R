ad1d9d337f1a47da8b75737255e9db228cc338c9
Aaron Schroeder <dads2busy@gmail.com>
2022-05-18 14:07:05 +0000

    ni simplify health dist

--- src/01_data/va_geo_vhd_2020_health_districts/02_prepare.R
+++ src/01_data/va_geo_vhd_2020_health_districts/02_prepare.R
@@ -30,10 +30,10 @@ va_geo_vhd_2020_health_districts$year <- "2020"
 final_dataset <- va_geo_vhd_2020_health_districts[, c("geoid", "region_name", "region_type", "year", "geometry")]
 
 # Simplify the geography
-final_dataset_simplified <- rmapshaper::ms_simplify(final_dataset, keep_shapes = TRUE)
+# final_dataset_simplified <- rmapshaper::ms_simplify(final_dataset, keep_shapes = TRUE, input = )
 
 # Export final dataset
-sf::st_write(final_dataset_simplified, "data/va_geo_vhd_2020_health_districts/distribution/va_geo_vhd_2020_health_districts.geojson")
+sf::st_write(final_dataset, "data/va_geo_vhd_2020_health_districts/distribution/va_geo_vhd_2020_health_districts.geojson")
 
 # Update file manifest
 data_file_checksums()
