333c522533528b46007779d627e2c099421bdb43
Aaron Schroeder <dads2busy@gmail.com>
2022-05-25 11:56:48 +0000

    arlington civ assoc id fix

--- src/01_data/va013_geo_arl_2021_civic_associations/02_prepare.R
+++ src/01_data/va013_geo_arl_2021_civic_associations/02_prepare.R
@@ -4,7 +4,7 @@
 va013_geo_arl_2021_civic_associations <- sf::st_read("data/va013_geo_arl_2021_civic_associations/original/va013_geo_arl_2021_civic_associations.geojson")
 
 # Assign geoid
-va013_geo_arl_2021_civic_associations$geoid <- paste0("51013_ca_", stringr::str_pad(va013_geo_arl_2021_civic_associations$OBJECTID, 2, side = "left", pad = "0"))
+va013_geo_arl_2021_civic_associations$geoid <- paste0("51013_ca_", stringr::str_pad(rownames(va013_geo_arl_2021_civic_associations), 2, side = "left", pad = "0"))
 
 # Assign region_type
 va013_geo_arl_2021_civic_associations$region_type <- "civic association"
